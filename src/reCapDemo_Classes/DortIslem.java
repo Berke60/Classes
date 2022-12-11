d0d6de682c1e209d809657991d4e52adca892be6
vatan <vatan @LAPTOP-SU8E7320>
2022-12-11 18:05:12 +0300

    Signed-off-by: vatan  <vatan @LAPTOP-SU8E7320>

---
+++ src/reCapDemo_Classes/DortIslem.java
@@ -0,0 +1,25 @@
+package reCapDemo_Classes;
+
+public class DortIslem {
+
+	public static void main(String[] args) {
+
+	}
+
+	public int Topla(int sayi1, int sayi2) {
+		return sayi1 + sayi2;
+	}
+
+	public int Çıkar(int sayi1, int sayi2) {
+		return sayi1 - sayi2;
+	}
+
+	public int Çarp(int sayi1, int sayi2) {
+		return sayi1 * sayi2;
+	}
+
+	public int Böl(int sayi1, int sayi2) {
+		return sayi1 / sayi2;
+	}
+
+}
---
+++ src/reCapDemo_Classes/Main.java
@@ -0,0 +1,12 @@
+package reCapDemo_Classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem dörtIslem = new DortIslem();
+		int sonuc = dörtIslem.Topla(2, 7);
+		System.out.println(sonuc);
+
+	}
+
+}
